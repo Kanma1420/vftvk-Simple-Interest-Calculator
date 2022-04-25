a56ba725b21ed619d7d525f4101eefa83adf56b6
Kanma1420 <oriakuogeya@gmail.com>
2022-04-25 15:53:40 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ function compute(){
     var interest = principal * years * rate /100;
     var amount = principal + interest;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + years + "</mark>";
+
 }
+
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 
 }
-    
 
-        
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        principal.focus();
+        document.getElementById("principal").focus();
+    }
+
+}
+
